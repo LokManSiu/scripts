29876ea9ccf84769a3917282a5df873ccf2fb9e9
bjsiu <bjsiu0617@gmail.com>
2016-04-08 23:28:19 +0800

    Create clean.sh

---
+++ clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -rf vendor/mediatek
+rm -rf vendor/xiaomi/hermes
+rm -rf packages/apps/Settings
+rm -rf device/xiaomi/hermes
+rm -rf system/core
+rm -rf system/netd
+rm -rf hardware/libhardware
+rm -rf hardware/libhardware/legacy
