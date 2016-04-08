88381bf185f710f340182775e50495d7b0346073
bjsiu <bjsiu0617@gmail.com>
2016-04-08 23:45:35 +0800

    Update recovery.sh

--- recovery.sh
+++ recovery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 rm -rf bootable/recovery
 git clone https://github.com/bjsiu/android_bootable_recovery.git bootable/recovery
-git clone https://github.com/bjsiu/android_device_xiaomi_hermes.git device/xiaomi/hermes
+git clone https://github.com/bjsiu/android_device_xiaomi_hermes-1.git device/xiaomi/hermes
