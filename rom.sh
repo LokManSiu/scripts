5afb3f1930c2f819b8bf929d45f819b630fe64cd
bjsiu <bjsiu0617@gmail.com>
2016-04-08 22:32:00 +0800

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+git clone https://github.com/bjsiu/android_vendor_mediatek.git -b cm-12.1 vendor/mediatek
+git clone https://github.com/bjsiu/android_vendor_xiaomi_hermes.git -b cm-12.1 vendor/xiaomi/hermes
+git clone https://github.com/bjsiu/android_packages_apps_Settings.git -b cm-12.1 packages/apps/Settings
+git clone https://github.com/bjsiu/android_device_xiaomi_hermes.git -b cm-12.1 device/xiaomi/hermes
+git clone https://github.com/bjsiu/android_system_core.git -b cm-12.1 system/core
+git clone https://github.com/bjsiu/android_system_netd.git -b cm-12.1 system/netd
+git clone https://github.com/bjsiu/android_hardware_libhardware.git -b cm-12.1 hardware/libhardware
+git clone https://github.com/bjsiu/android_hardware_libhardware_legacy.git -b cm-12.1 hardware/libhardware/legacy
