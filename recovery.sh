a2f1cca0006d0d173c6717c32dca8e9033889cb6
bjsiu <bjsiu0617@gmail.com>
2016-04-09 17:27:42 +0800

    Delete recovery.sh

--- recovery.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf bootable/recovery
-git clone https://github.com/bjsiu/android_bootable_recovery.git bootable/recovery
-git clone https://github.com/bjsiu/android_device_xiaomi_hermes-1.git device/xiaomi/hermes
