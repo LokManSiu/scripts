a780ccb591e7f6a846b525d35855b100d2090336
bjsiu <bjsiu0617@gmail.com>
2016-04-09 17:45:19 +0800

    Create sync.sh

---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+repo sync --force-sync vendor/mediatek
+repo sync --force-sync vendor/xiaomi/hermes
+repo sync --force-sync packages/apps/Settings
+repo sync --force-sync device/xiaomi/hermes
+repo sync --force-sync system/core
+repo sync --force-sync system/netd
+repo sync --force-sync hardware/libhardware
+repo sync --force-sync hardware/libhardware_legacy
+repo sync --force-sync bootable/recovery
+repo sync -j8
